d39282dc0ab0363ae27643645621ed433bac6a41
mpan-wework <mike.pan@wework.com>
2020-09-16 15:43:13 +0800

    feat(config): .zshrc

--- home/install.sh
+++ home/install.sh
@@ -3,8 +3,16 @@
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo Script directory: $SCRIPT_DIR
 
+# ~/.zshrc
+ZSHRC=.zshrc
+if [ -L ~/$ZSHRC ]; then rm ~/$ZSHRC; fi
+if [ -f ~/$ZSHRC ]; then mv ~/$ZSHRC ~/$ZSHRC.bak; fi
+ln -sf $SCRIPT_DIR/$ZSHRC ~/$ZSHRC
+ls -al ~/$ZSHRC
+
 # ~/.vimrc
-if [ -L ~/.vimrc ]; then rm ~/.vimrc; fi
-if [ -f ~/.vimrc ]; then mv ~/.vimrc ~/.vimrc.bak; fi
-ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
-ls -al ~/.vimrc
+VIMRC=.vimrc
+if [ -L ~/$VIMRC ]; then rm ~/$VIMRC; fi
+if [ -f ~/$VIMRC ]; then mv ~/$VIMRC ~/$VIMRC.bak; fi
+ln -sf $SCRIPT_DIR/$VIMRC ~/$VIMRC
+ls -al ~/$VIMRC
